ecf5e0479025c80eff9ec05ae2098b07e5c18a59
Sambillingham <sam.billingham@steer.io>
2015-08-25 18:07:05 +0100

    local jekyll dev with browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,10 @@ var rename = require('gulp-rename');
 var prefix = require('gulp-autoprefixer');
 var ghBuild = require('gulp-gh-pages');
 var clean = require('gulp-clean');
+var browserSync = require('browser-sync');
+var spawn = require('child_process').spawn;
 
-gulp.task('jekyll', function (gulpCallBack){
-   var spawn = require('child_process').spawn;
+gulp.task('jekyll-prod', function (gulpCallBack){
    var enviroment = process.env
    enviroment['JEKYLL_ENV'] = 'production';
    var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit', env: enviroment});
@@ -21,7 +22,18 @@ gulp.task('jekyll', function (gulpCallBack){
    });
 });
 
-gulp.task('clean-prod', ['jekyll'], function(){
+gulp.task('jekyll-dev', function (done) {
+    browserSync.notify('Running: $ jekyll build - This takes awhile...');
+
+    spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('clean-prod', ['jekyll-prod'], function(){
   return gulp.src(['_site/css/*', '_site/js/*'], {read: false})
         .pipe(clean());
 });
@@ -48,29 +60,34 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('reload-js', ['lint'], function(){
+  gulp.src('js/main.js')
+      .pipe(gulp.dest('_site/js'))
+      .pipe(browserSync.reload({stream:true}))
+})
+
 gulp.task('sass', function() {
     return sass('sass/', { style: 'expanded' })
         .pipe(prefix("last 3 version", "> 1%", "ie 8"))
         .pipe(rename('main.css'))
-        .pipe(gulp.dest('css/'))
-        .pipe(rename('main.min.css'))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('css/'))
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}))
 });
 
-gulp.task('scripts', function() {
-    gulp.src(['js/jquery-1.11.3.min.js', 'js/picturefill.min.js', 'js/svgeezy.min.js', 'js/selectivizr-min.js', 'js/main.js'])
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('js/'))
-        .pipe(rename('main.min.js'))
-        .pipe(uglify({outSourceMap: true}))
-        .pipe(gulp.dest('js/'));
+gulp.task('browser-sync', ['sass', 'jekyll-dev'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
 });
 
 gulp.task('watch', function() {
-    gulp.watch('js/main.js', ['lint', 'scripts']);
+    gulp.watch('js/main.js', ['lint', 'reload-js']);
     gulp.watch('sass/**/{*.sass,*.scss}', ['sass']);
+    gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
-gulp.task('default', ['watch']);
-gulp.task('build', ['jekyll', 'clean-prod','prod-sass', 'prod-js']);
+gulp.task('dev', ['browser-sync', 'watch']);
+gulp.task('build', ['jekyll-prod', 'clean-prod','prod-sass', 'prod-js']);
+gulp.task('default', ['dev']);
