65d2214046ef7927e820e3abb47cbb0e4513a49c
Sambillingham <sam.billingham@steer.io>
2015-10-01 16:36:06 +0100

    :cookie: split js into modules

---
+++ js/modules/mailchimp-form.js
@@ -0,0 +1,25 @@
+class MailchimpForm {
+  reister(form){
+    $.ajax({
+      type: form.attr('method'),
+      url: form.attr('action'),
+      data: form.serialize(),
+      cache : false,
+      dataType : 'json',
+      contentType: 'application/json; charset=utf-8',
+      complete : function(data) {
+
+        var message = data.responseJSON.msg;
+
+        if (data.result !== 'success') {
+          console.log(data);
+          $('.sign-up__message').addClass('sign-up__message--active sign-up__message--fail').html(message);
+        } else {
+          console.log(data);
+          $('.sign-up__message').addClass('sign-up__message--active sign-up__message--success').html(message);
+        }
+      }
+    }); // end AJAX
+  }
+}
+export default new MailchimpForm();
---
+++ js/modules/ui-actions.js
@@ -0,0 +1,35 @@
+import speakerModal from './speaker-modal';
+import mailchimpForm from './mailchimp-form';
+
+class UiActions {
+  init() {
+    var form = $('#mc-embedded-subscribe-form');
+
+    $('.sign-up__submit').on('click', function ( event ) {
+      mailchimpForm.mailChimpAjaxRegister(form);
+      event.preventDefault();
+    });
+
+    $('.schedule__session').not('.schedule__session--break, .schedule__session--coming-soon, .schedule__session--gap ').on('click', function(event) {
+      var slot = this;
+      speakerModal.scheduleSPeakerInfo(slot);
+      event.preventDefault();
+    });
+
+    $('.js-section-header-modal').on('click', function(){
+      $('body').removeClass('lock-scroll');
+      $('.js-schedule__full-details, .schedule-session-details-overlay').fadeOut();
+    });
+
+    $(window).on('resize', function(){
+
+      if(Modernizr.mq('only screen and (max-width: 850px)')) {
+        $('.js-slot-info').css('margin-top', 0);
+      } else {
+        $('.js-section-header-modal').empty();
+      }
+
+    });
+  }
+}
+export default new UiActions();
