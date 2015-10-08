f7d58f29848a9df3d7ea4662d752b2506fc9c595
Sambillingham <sam.billingham@steer.io>
2015-10-08 11:37:58 +0100

    :cookie: init fitvid.js on blog content videos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 "use strict";
+//vendor
+import fitvid from '../bower_components/fitvids/jquery.fitvids.js';
+
+//modules
 import countdown from './modules/countdown';
 import uiActions from './modules/ui-actions';
 import schedule from './modules/schedule';
 
 const APP = {
   init: function() {
+    // vendor init
+    $('.blog-content-video').fitVids();
     svgeezy.init(false, 'png');
+
+    //Module init
     uiActions.init();
     countdown.init();
     schedule.init();
