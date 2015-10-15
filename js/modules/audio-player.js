0c2427df06e771c6c03a327d33528ea1278f4a61
Sam Mason <sam.jbmason@gmail.com>
2015-10-15 15:49:05 +0100

    Create audio player file and do some setup

---
+++ js/modules/audio-player.js
@@ -0,0 +1,28 @@
+class AudioPlayer {
+  init() {
+    console.log("test")
+    this.setupPlayer()
+  }
+
+  setupPlayer() {
+    $('.js-audio-player-wrapper').jPlayer({
+      ready: function() {
+        $(this).jPlayer('setMedia', {
+          mp3: $('.js-audio-player-wrapper').data('audio')
+        });
+      },
+      preload: 'auto',
+      volume: 1,
+      cssSelectorAncestor: '.player__container',
+      cssSelector: {
+        play: '.play-btn',
+        pause: '.pause-btn',
+        seekBar: '.player__seek-bar',
+        playBar: '.player__play-bar',
+        currentTime: '.player__current-time',
+        duration: '.player__duration'
+      }
+    });
+  }
+}
+export default new AudioPlayer();
